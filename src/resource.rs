dbe995c6640a7a08b9b887cabf66a336d9180f73
pfried <philipsfried@gmail.com>
2019-12-04 23:28:57 -0500

    add some doc comments to handler and resource modules

--- src/resource.rs
+++ src/resource.rs
@@ -37,13 +37,18 @@ impl fmt::Display for InvalidResourceError {
 
 impl std::error::Error for InvalidResourceError {}
 
+/// A `K8sResource` represents a Kubernetes resource that actually exists in the cluster.
+/// It is a transparent wrapper around a `serde_json::Value`, with some validation and some
+/// accessors for common properties.
+/// This type is _not_ used to represent resources that haven't been created yet, since
+/// the fields that are populated by the api server would be missing.
 #[derive(PartialEq, Clone, Serialize, Deserialize)]
 #[serde(transparent)]
 pub struct K8sResource(Value);
 
 impl Debug for K8sResource {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.0)
+        write!(f, "K8sResource({})", self.0)
     }
 }
 
@@ -65,55 +70,69 @@ impl K8sResource {
         }
     }
 
+    /// unwrap the resource into the raw json Value
     pub fn into_value(self) -> Value {
         self.0
     }
 
+    /// returns the `metadata.resourceVersion`, which is guaranteed to exist
     pub fn get_resource_version(&self) -> &str {
         self.str_value("/metadata/resourceVersion").unwrap()
     }
 
+    /// returns the value of the given label, if it exists
     pub fn get_label_value(&self, label: &str) -> Option<&str> {
         let labels = self.0.pointer("/metadata/labels")?.as_object()?;
         labels.get(label).and_then(Value::as_str)
     }
 
+    /// returns the `metadata.uid`, which is guaranteed to exist
     pub fn uid(&self) -> &str {
         self.str_value("/metadata/uid").unwrap()
     }
 
+    /// returns the `metadata.name`, which is guaranteed to exist
     pub fn name(&self) -> &str {
         self.str_value("/metadata/name").unwrap()
     }
 
+    /// returns the `metadata.namespace`, which will only exist for namespaced resources
     pub fn namespace(&self) -> Option<&str> {
         self.str_value("/metadata/namespace")
     }
 
+    /// returns the `apiVersion` of the resource, which is guaranteed to exist
     pub fn api_version(&self) -> &str {
         self.str_value("/apiVersion").unwrap()
     }
 
+    /// returns the `kind` of the resource, which is guaranteed to exist
     pub fn kind(&self) -> &str {
         self.str_value("/kind").unwrap()
     }
 
+    /// returns the status of the resource, which may be missing. No validation is done on the
+    /// status, so callers must not make any assumptions about the `Value`, which may not be an
+    /// object, or may even be `Value::Null`
     pub fn status(&self) -> Option<&Value> {
         self.0.pointer("/status")
     }
 
+    /// Returns the id of this object as a pair of namespace/name
     pub fn get_object_id(&self) -> ObjectIdRef {
         let ns = self.namespace().unwrap_or("");
         let name = self.name();
         ObjectIdRef::new(ns, name)
     }
 
+    /// returns the type of the resource as a pair of apiVersion/kind
     pub fn get_type_ref(&self) -> K8sTypeRef {
         let api_version = self.api_version();
         let kind = self.kind();
         K8sTypeRef(api_version, kind)
     }
 
+    /// returns the current value of `metadata.generation`
     pub fn generation(&self) -> i64 {
         self.0
             .pointer("/metadata/generation")
@@ -121,6 +140,8 @@ impl K8sResource {
             .unwrap_or(-1)
     }
 
+    /// returns true if `metadata.deletionTimestamp` is set, which would indicate
+    /// that the resource is in the process of being deleted
     pub fn is_deletion_timestamp_set(&self) -> bool {
         self.0.pointer("/metadata/deletionTimestamp").is_some()
     }
